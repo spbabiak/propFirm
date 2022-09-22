e1d1efb59b3875c848dbba3727b1f9f59715c5d7
Sergey Babiak <spbabiak@gmail.com>
2022-09-22 11:32:47 +0300

    add mobile and tablet version of the home page

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const testimonialSlider = document.querySelector('.testimonials-slider')
 const leftArrow = document.querySelector('.left')
 const rightArrow = document.querySelector('.right')
 const circleControlsWrapper = document.querySelector('.facts-carousel .controls .circles')
-const testimonialCirclesWrapper = document.querySelector('.testimonials-carousel .controls .circles')
+const testimonialCirclesWrapper = document.querySelector('.testimonials .controls .circles')
 
 menuBtn.addEventListener('click', event => {
 	if(getComputedStyle(navVertical).display == 'none') {
@@ -33,60 +33,55 @@ programsItem.addEventListener('click', event => {
 })
 
 // Facts Slider
+let width = screen.width;
 
 let slide = 0
-let slidesCount = slider.children.length - 1
+
+if(width < 960) {
+	let slidesCount = slider.children.length - 1
+	let slideWidth = 100 / (slidesCount + 1)
+
+	slider.style.width = (slidesCount + 1) * 100 + '%'
+
+	function moveSlide() {
+	    slider.style.transform = 'translate(' + slide * -slideWidth + '%)'
+	}
+
+	rightArrow.onclick = () => {
+	    slide = (slide < slidesCount) ? slide + 1 : 0
+	    moveSlide()
+	}
+
+	leftArrow.onclick = () => {
+	    slide = (slide > 0) ? slide - 1 : slidesCount
+	    moveSlide()
+	}
+
+} else {
+	leftArrow.style.display = "none"
+	rightArrow.style.display = "none"
+}
+
+// Testimonials Slider 
+
 let testimonialSlidesCount = testimonialSlider.children.length - 1
-let slideWidth = 100 / (slidesCount + 1)
 let testimonialSlideWidth = 100 / (testimonialSlidesCount + 1)
 
-slider.style.width = (slidesCount + 1) * 100 + '%'
 testimonialSlider.style.width = (testimonialSlidesCount + 1) * 100 + '%'
-
-for(let i = 0; i <= slidesCount; i++) {
-    let circleElement = document.createElement('li')
-    circleControlsWrapper.appendChild(circleElement)
-}
 
 for(let i = 0; i <= testimonialSlidesCount; i++) {
     let circleElement = document.createElement('li')
     testimonialCirclesWrapper.appendChild(circleElement)
 }
 
-const circleControls = document.querySelectorAll('.facts-carousel .controls .circles li')
-circleControlsWrapper.firstChild.classList.add('active')
-
-const testimonialCircleControls = document.querySelectorAll('.testimonials-carousel .controls .circles li')
+const testimonialCircleControls = document.querySelectorAll('.testimonials .controls .circles li')
 testimonialCirclesWrapper.firstChild.classList.add('active')
-
-function moveSlide() {
-    slider.style.transform = 'translate(' + slide * -slideWidth + '%)'
-    circleControlsWrapper.querySelector('.active').classList.remove('active')
-    circleControls[slide].classList.add('active')
-}
 
 function moveTestimonialSlide() {
 	testimonialSlider.style.transform = 'translate(' + slide * -testimonialSlideWidth + '%)'
     testimonialCirclesWrapper.querySelector('.active').classList.remove('active')
     testimonialCircleControls[slide].classList.add('active')	
 }
-
-rightArrow.onclick = () => {
-    slide = (slide < slidesCount) ? slide + 1 : 0
-    moveSlide()
-}
-
-leftArrow.onclick = () => {
-    slide = (slide > 0) ? slide - 1 : slidesCount
-    moveSlide()
-}
-
-circleControls.forEach((circle, index) => {
-    circle.onclick = () => {
-        slide = index
-        moveSlide()
-    }
-})
 
 testimonialCircleControls.forEach((circle, index) => {
     circle.onclick = () => {
