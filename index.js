1f298d78bfdb3149e095e59f50d3bab47565be06
Sergey Babiak <spbabiak@gmail.com>
2022-09-23 19:41:58 +0300

    add tabel switchers for second section on the home page

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Dropdown Menu Item -----------------------------------//
+// Mobile Menu ------------------------------------------//
+// Currency Switcher Mobile -----------------------------//
+// Switcher Controls ------------------------------------//
+// Facts Slider -----------------------------------------//
+// Testimonials Slider ----------------------------------//
+
 const menuBtn = document.querySelector('.menu-btn-wrapper')
 const navVertical = document.querySelector('.nav-vertical')
 const closeBtn = document.querySelector('.close-btn')
@@ -12,6 +19,65 @@ const rightArrow = document.querySelector('.right')
 const circleControlsWrapper = document.querySelector('.facts-carousel .controls .circles')
 const testimonialCirclesWrapper = document.querySelector('.testimonials .controls .circles')
 
+const switcherContol = document.querySelectorAll('.switcher-controls')
+const switcherContolsArray = Array.from(switcherContol)
+
+const currencySwitcherContolMobile = document.querySelectorAll('.currency-switcher-mobile .switcher-controls')
+const currencySwitcherItemsMobileArray = Array.from(currencySwitcherContolMobile)
+let currencyArray = []
+
+
+// Switcher Controls -------------------------------------------------------------------------------
+switcherContolsArray.forEach(item => {
+	let switcherContolItems = item.querySelectorAll('li')
+	let switcherContolItemsArray = Array.from(switcherContolItems)
+
+	switcherContolItemsArray.forEach((item, index) => {
+		item.addEventListener('click', event => {
+			for(let i = 0; i < switcherContolItemsArray.length; i++) {
+				if(switcherContolItemsArray[i].classList == 'active') {
+					switcherContolItemsArray[i].classList.remove('active')		
+				}
+			}
+
+			if(item.classList !== 'active') {
+				item.classList.add('active')
+			}
+		})
+	})	
+})
+
+// Switcher Controls End ---------------------------------------------------------------------
+
+// Currency Switcher Mobile ---------------------------------------------------------------------
+currencySwitcherContolMobile.forEach(item => {
+	const currencyItems = item.querySelectorAll('li')
+	const currencyItemsArray = Array.from(currencyItems)
+	const items = currencyArray.push(currencyItemsArray)
+})
+
+let wholeCurrencyArray
+for(let i = 0; i < currencyArray.length - 1; i++) {
+	wholeCurrencyArray = currencyArray[i].concat(currencyArray[i+1])
+}
+
+wholeCurrencyArray.forEach(item => {
+	item.addEventListener('click', event => {
+		for(let i = 0; i < wholeCurrencyArray.length; i++) {
+			if(wholeCurrencyArray[i].classList == 'active') {
+				wholeCurrencyArray[i].classList.remove('active')		
+			}
+		}
+
+		if(item.classList !== 'active') {
+			item.classList.add('active')
+		}
+	})
+})
+
+// Currency Switcher Mobile End -----------------------------------------------------------------------------------------
+
+// Mobile Menu -----------------------------------------------------------------------------------------
 menuBtn.addEventListener('click', event => {
 	if(getComputedStyle(navVertical).display == 'none') {
 		navVertical.style.display = 'block'
@@ -24,6 +90,9 @@ closeBtn.addEventListener('click', event => {
 	navVertical.style.display = 'none'	
 })
 
+// Mobile Menu End -----------------------------------------------------------------------------------------
+
+// Dropdown Menu item -----------------------------------------------------------------------------------------
 programsItem.addEventListener('click', event => {
 	if(getComputedStyle(programsList).display == 'none') {
 		programsList.style.display = 'block'
@@ -32,7 +101,9 @@ programsItem.addEventListener('click', event => {
 	}	
 })
 
-// Facts Slider
+// Dropdown Menu item End --------------------------------------------------------------------------
+
+// Facts Slider ------------------------------------------------------------------------------------
 let screenWidth = screen.width;
 
 let slide = 0
@@ -61,9 +132,9 @@ if(screenWidth < 960) {
 	leftArrow.style.display = "none"
 	rightArrow.style.display = "none"
 }
+// Facts Slider End ------------------------------------------------------------------------------------
 
-// Testimonials Slider 
-
+// Testimonials Slider ------------------------------------------------------------------------------
 let testimonialSlidesCount = testimonialSlider.children.length - 1
 let testimonialSlideWidth = 100 / (testimonialSlidesCount + 1)
 
@@ -91,3 +162,6 @@ testimonialCircleControls.forEach((circle, index) => {
         moveTestimonialSlide()
     }
 })
+
+// Testimonials Slider End ------------------------------------------------------------------------------
+
